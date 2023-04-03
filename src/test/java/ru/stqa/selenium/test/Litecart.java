996228a6acd187bff1e7b729704f7b79b88f1829
ISmirnov-debug <128966783+ISmirnov-debug@users.noreply.github.com>
2023-04-03 17:45:01 +0300

    Task№2-№3 (LogonLitecart)

---
+++ src/test/java/ru/stqa/selenium/test/Litecart.java
@@ -0,0 +1,41 @@
+package ru.stqa.selenium.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class Litecart {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Test
+    public void LitecartLog() {
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
+    }
+    public void start(){
+        ChromeOptions o = new ChromeOptions();
+        o.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(o);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
